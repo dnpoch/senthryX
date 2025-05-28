af6b26bb1e7bbf78cc983217ac0ccee20bd6df31
Don Pochulo Tidor <pochulotidor@gmail.com>
2025-05-29 01:36:09 +0800

    add new config field

--- src/main/java/com/pochixcx/util/Config.java
+++ src/main/java/com/pochixcx/util/Config.java
@@ -5,9 +5,10 @@ import java.util.ArrayList;
 public class Config {
     public boolean activate = true;
     public boolean enable_discord = true;
-    public String bot_token = "";
-    public String admin_channel_id = "";
-    public ArrayList<String> logging_channels = new ArrayList<String>();
+    public String bot_token = ""; // supply your discord bot token here if enable_discord is true
+    public String admin_channel_id = ""; // supply a discord channel ID for admin commands if enable_discord is true
+    public boolean enable_public_logging = true;
+    public String public_log_channel = ""; // supply a discord channel ID if enable_public_logging is true
     public ArrayList<String> admin_ids = new ArrayList<String>();
     public String presence = "Minecraft";
     public String kick_message = "You are not whitelisted";
